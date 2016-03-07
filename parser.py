d50aa94ba375f3cf692fbc951292e0994ea5d6f3
Colin Schoen <cschoen@berkeley.edu>
2016-03-07 15:10:21 -0800

    Continue implementing parser
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- parser.py
+++ parser.py
@@ -1,11 +1,20 @@
+class Connection:
+    def __init__(self, cid, server, callsign, client, uid):
+        self.cid = cid
+        self.server = server
+        self.callsign = callsign
+        self.client = client
+        self.uid = uid
+
 class Parser:
     def __init__(self, logs, debug=False):
         assert type(logs) is str or type(logs) is list, "Log must be a string or list."
         #Create some of our empty instance variables
-        self.connections = {}
+        self.server2connections = {}
+        self.connections = []
         #If log is a string convert it to an iterable (list)
         if type(logs) is str:
-            self.logs = list(logs)
+            self.logs = [logs]
         else:
             self.logs = logs
         #Read our logs
@@ -19,5 +28,26 @@ class Parser:
                         continue
                     #Split our string
                     ldata = l.rsplit('Client logged in: ', 1)[1].split(':')
+                    #callsign,server,unknown,client,unknown,unknown
+                    connection = Connection(ldata[6], ldata[1], ldata[0], ldata[3], ldata[7])
+                    #Add our connection
+                    if ldata[0] in self.server2connections:
+                        self.server2connections[ldata[0]].append(connection)
+                    else:
+                        self.server2connections[ldata[0]] = []
+                        self.server2connections[ldata[0]].append(connection)
+                    self.connections.append(connection)
+    def get_connections(self):
+        #Copy our list so the original can't be mutated
+        return list(self.connections)
+
+    def get_connections_by_server(self, server=None):
+        #Copy our dictionatry so the original can't be mutated
+        if server:
+            return dict(self.server2connections[server])
+        return dict(self.server2connections)
+                    
+
+
                     
 
